86c6591f36bb8d9cd3f9662f4d662836d7f05516
rflewandowski <r.f.lewandowski@gmail.com>
2024-03-04 21:32:30 +0100

    fixed css for base components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {WorkspaceComponent} from "./workspace/workspace.component";
-import {NavbarComponent} from "./navbar/navbar.component";
+import {NavbarComponent} from "./workspace/navbar/navbar.component";
 import {SheetComponent} from "./workspace/sheet/sheet.component";
 
 @Component({
