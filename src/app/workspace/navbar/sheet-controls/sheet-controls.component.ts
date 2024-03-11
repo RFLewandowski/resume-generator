44d074be7d6f05d5ed499d5fcf2bfe5931b8d3f5
rflewandowski <r.f.lewandowski@gmail.com>
2024-03-12 00:49:49 +0100

    added dropdowns

--- src/app/workspace/navbar/sheet-controls/sheet-controls.component.ts
+++ src/app/workspace/navbar/sheet-controls/sheet-controls.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SectionsComponent} from "./sections/sections.component";
 
 @Component({
   selector: 'app-sheet-controls',
   standalone: true,
-  imports: [],
+  imports: [
+    SectionsComponent
+  ],
   templateUrl: './sheet-controls.component.html',
   styleUrl: './sheet-controls.component.scss'
 })
