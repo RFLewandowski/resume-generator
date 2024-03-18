c58890c86e10144ca82f7f9cb663b48816c7298c
rflewandowski <r.f.lewandowski@gmail.com>
2024-03-18 01:42:19 +0100

    added comment

--- src/app/workspace/navbar/sheet-controls/color-control/color-control.component.ts
+++ src/app/workspace/navbar/sheet-controls/color-control/color-control.component.ts
@@ -19,6 +19,7 @@ export class ColorControlComponent {
 
   pickedColor: any; // FIXME to działa ale jest bez sensu, bo z pickera nie ma konkretnej wartości z listy -
   // więc nie można sprawdzić tak prosto czy coś jest kolorem z enuma - bo może nie być z enuma
+  // najlepiej przerobić na wyciąganie z evenu
 
   get colors(): Color[] {
     return Object.values(Color);
