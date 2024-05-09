b934f8edb9b7386bfba0a16d03e1aeb112464d95
rflewandowski <r.f.lewandowski@gmail.com>
2024-05-09 20:43:17 +0200

    move of component to example section code will be copied and pasted

--- src/app/experiment/pf-dropdown/pf-dropdown.component.ts
+++ src/app/experiment/pf-dropdown/pf-dropdown.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {SvgIconComponent} from "../../../svg-icon/svg-icon.component";
+import {SvgIconComponent} from "../../svg-icon/svg-icon.component";
 import {StyleClassModule} from "primeng/styleclass";
 
 @Component({
--- src/app/workspace/navbar/sheet-controls/sheet-controls.component.ts
+++ src/app/workspace/navbar/sheet-controls/sheet-controls.component.ts
@@ -3,9 +3,6 @@ import {LayoutControlComponent} from "./layout-control/layout-control.component"
 import {SectionsControlComponent} from "./sections-control/sections-control.component";
 import {ColorControlComponent} from "./color-control/color-control.component";
 import {TypographyControlComponent} from "./typography-control/typography-control.component";
-import {ButtonModule} from "primeng/button";
-import {SvgIconComponent} from "../../../svg-icon/svg-icon.component";
-import {PfDropdownComponent} from "../pf-dropdown/pf-dropdown.component";
 
 @Component({
   selector: 'app-sheet-controls',
@@ -14,15 +11,10 @@ import {PfDropdownComponent} from "../pf-dropdown/pf-dropdown.component";
     LayoutControlComponent,
     SectionsControlComponent,
     ColorControlComponent,
-    TypographyControlComponent,
-    ButtonModule,
-    SvgIconComponent,
-    PfDropdownComponent
+    TypographyControlComponent
   ],
   templateUrl: './sheet-controls.component.html',
   styleUrl: './sheet-controls.component.scss'
 })
 export class SheetControlsComponent {
-// FIXME tutaj będzie sporo importów do wywalania, bo to będzie tylko kontener na buttony menu
-
 }
