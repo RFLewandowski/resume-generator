a4400aefabc382330f7cda136c03a31ccb7b89cb
rflewandowski <r.f.lewandowski@gmail.com>
2024-05-09 20:48:00 +0200

    removed menubar example from app

--- src/app/workspace/sheet/sheet.component.ts
+++ src/app/workspace/sheet/sheet.component.ts
@@ -1,15 +1,11 @@
-import { Component } from '@angular/core';
-import {MenubarExampleComponent} from "../../experiment/menubar-example/menubar-example.component";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sheet',
   standalone: true,
-  imports: [
-    MenubarExampleComponent
-  ],
+  imports: [],
   templateUrl: './sheet.component.html',
   styleUrl: './sheet.component.scss'
 })
 export class SheetComponent {
- // TODO remove menu MenubarExampleComponent when not required
 }
