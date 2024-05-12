ab0d9aebab3e75ad630c162e17cb8545ec1ac008
rflewandowski <r.f.lewandowski@gmail.com>
2024-05-12 23:17:36 +0200

    started working on colors

--- src/app/workspace/navbar/sheet-controls/color-control/color-control.component.ts
+++ src/app/workspace/navbar/sheet-controls/color-control/color-control.component.ts
@@ -3,19 +3,24 @@ import {NgForOf} from "@angular/common";
 import {SheetStateService} from "../../../../services/sheet-state.service";
 import {Color, ShadeOfGray} from "./color";
 import {FormsModule} from "@angular/forms";
+import {SvgIconComponent} from "../../../../svg-icon/svg-icon.component";
+import {ColorPickerModule} from "primeng/colorpicker";
 
 @Component({
   selector: 'app-color-control',
   standalone: true,
   imports: [
     NgForOf,
-    FormsModule
+    FormsModule,
+    SvgIconComponent,
+    ColorPickerModule
   ],
   templateUrl: './color-control.component.html',
   styleUrl: './color-control.component.scss'
 })
 export class ColorControlComponent {
   protected readonly ShadeOfGray = ShadeOfGray;
+  protected color: string | undefined;// test picker
 
   constructor(private sheetStateService: SheetStateService) {
   }
