024f7cd16a309f278a97e6ce2a9741193e6bdbdf
rflewandowski <r.f.lewandowski@gmail.com>
2024-05-15 23:27:28 +0200

    set up some work for sections

--- src/app/workspace/navbar/sheet-controls/sections-control/sections-control.component.ts
+++ src/app/workspace/navbar/sheet-controls/sections-control/sections-control.component.ts
@@ -3,14 +3,22 @@ import {NgForOf} from "@angular/common";
 import {SectionItem} from "./section-item";
 import {SheetStateService} from "../../../../services/sheet-state.service";
 import {CustomSectionSelectorComponent} from "./custom-section-selector/custom-section-selector.component";
+import {ButtonGroupModule} from "primeng/buttongroup";
+import {ButtonModule} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+import {SvgIconComponent} from "../../../../svg-icon/svg-icon.component";
 
 @Component({
   selector: 'app-sections-control',
   standalone: true,
-  imports: [
-    NgForOf,
-    CustomSectionSelectorComponent
-  ],
+    imports: [
+        NgForOf,
+        CustomSectionSelectorComponent,
+        ButtonGroupModule,
+        ButtonModule,
+        DropdownModule,
+        SvgIconComponent
+    ],
   templateUrl: './sections-control.component.html',
   styleUrl: './sections-control.component.scss'
 })
